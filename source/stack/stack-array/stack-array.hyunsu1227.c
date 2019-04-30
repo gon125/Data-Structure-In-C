de1b7bbbb54875364554931f72912d517acd2fd3
Hyunsu <hyunsujang1227@gmail.com>
2019-05-01 01:38:54 +0900

    Add stack-array.hyunsu1227.c

---
+++ source/stack/stack-array/stack-array.hyunsu1227.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_STACK_SIZE 1001
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+	int data;
+}element;
+
+element stack[MAX_STACK_SIZE];
+int top = -1;
+
+void main()
+{
+	
+}
+void push(element item)
+{
+	top++;
+	if (top >= MAX_STACK_SIZE){
+		stack_full();
+	}
+	stack[top] = item;
+}
+void stack_full()
+{
+	fprintf(stderr, "stack is full");
+	exit(1);
+}
+element pop()
+{
+	if (top == -1) {
+		stack_empty();
+	}
+	return stack[top--];
+}
+void stack_empty()
+{
+	fprintf(stderr, "stack is empty");
+	exit(1);
+}
